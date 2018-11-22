61345dd1b906d04c4842348087f216b3991678f7
Jqnx <jan.derpingguys@hotmail.com>
2018-11-22 22:28:46 +0100

    Added more AMV's and added media queries to css files

---
+++ video.js
@@ -0,0 +1,18 @@
+var videoPlayer= document.getElementById('video');
+videoPlayer.volume = 0.1;
+
+function playIt(){
+videoPlayer.play();
+
+var videos = [
+"video1",
+"video2",
+"video3",
+], videos = videos[Math.floor(Math.random() * videos.length)];
+
+videoPlayer.src = "../css/videos/" + videos + ".mp4";
+}
+
+videoPlayer.addEventListener('ended', playIt, false);
+
+playIt();
